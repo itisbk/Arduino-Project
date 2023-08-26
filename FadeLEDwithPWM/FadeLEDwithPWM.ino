61802c78b4e46987e3e420bab0031204a0c97af5
itisbk <karthikeyan.boopathy@gmail.com>
2023-08-26 20:25:38 +0530

    Fade LED using anolog pin 9

---
+++ FadeLEDwithPWM/FadeLEDwithPWM.ino
@@ -0,0 +1,33 @@
+// C++ code
+//
+/*
+  Fade LED
+
+  This example shows how to fade an LED using
+  Analog pin 9
+
+  The analogWrite() function uses PWM, so if you
+  want to change the pin you're using, be sure to
+  use another PWM capable pin. On most Arduino,
+  the PWM pins are marked with a "~" sign,
+  like ~3, ~5, ~6, ~9, ~10 and ~11
+*/
+
+int brightness = 0;
+
+void setup()
+{
+  pinMode(9, OUTPUT);
+}
+
+void loop()
+{
+  for (brightness = 0; brightness <= 255; brightness += 5) {
+    analogWrite(9, brightness);
+    delay(30); // Wait for 30 millisecond(s)
+  }
+  for (brightness = 255; brightness >= 0; brightness -= 5) {
+    analogWrite(9, brightness);
+    delay(30); // Wait for 30 millisecond(s)
+  }
+}
